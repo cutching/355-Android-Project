3519cef7ac2a891a76be327a34be2c901cb5d8db
cutching <gcutchin011@gmail.com>
2021-12-14 01:37:24 -0500

    Added and Fixed all Settings Buttons
    Added and Fixed all Settings buttons.

--- Hive/app/src/main/java/com/example/hive/Calendar.java
+++ Hive/app/src/main/java/com/example/hive/Calendar.java
@@ -3,14 +3,29 @@ package com.example.hive;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Calendar extends AppCompatActivity {
+
+    Button callSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        callSettings = findViewById(R.id.settingsButton);
+        callSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Calendar.this,Settings.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- Hive/app/src/main/java/com/example/hive/Groceries.java
+++ Hive/app/src/main/java/com/example/hive/Groceries.java
@@ -2,13 +2,28 @@ package com.example.hive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Groceries extends AppCompatActivity {
+
+    Button callSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groceries);
+
+        callSettings = findViewById(R.id.settingsButton);
+        callSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Groceries.this,Settings.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
