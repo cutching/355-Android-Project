7b9ac10e94cb7f0680955bce123a086295bccb8a
cutching <gcutchin011@gmail.com>
2021-12-14 02:13:26 -0500

    Set up Settings
    Sets up settings page with format and classes.

--- Hive/app/src/main/java/com/example/hive/Settings.java
+++ Hive/app/src/main/java/com/example/hive/Settings.java
@@ -1,14 +1,33 @@
 package com.example.hive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Settings extends AppCompatActivity {
+
+    TextView fullHouseName;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
     }
+
+
 }
